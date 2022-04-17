a5cf2f671b4edaf26a5be908968d0dac23c28589
kyun9 <gusl108@naver.com>
2022-04-17 21:45:06 +0900

    update new prj

--- src/main/java/hello/hellospring2/HelloSpring2Application.java
+++ src/main/java/hello/hellospring2/HelloSpring2Application.java
@@ -1,13 +1,13 @@
-package hello.hellospring;
+package hello.hellospring2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloSpringApplication {
+public class HelloSpring2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringApplication.class, args);
+		SpringApplication.run(HelloSpring2Application.class, args);
 	}
 
 }
--- src/test/java/hello/hellospring2/HelloSpring2ApplicationTests.java
+++ src/test/java/hello/hellospring2/HelloSpring2ApplicationTests.java
@@ -1,10 +1,10 @@
-package hello.hellospring;
+package hello.hellospring2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelloSpringApplicationTests {
+class HelloSpring2ApplicationTests {
 
 	@Test
 	void contextLoads() {
