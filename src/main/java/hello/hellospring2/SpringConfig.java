e7d1187ffc93cb5b2badcabe385804cb4853bbfb
kyun9 <gusl108@naver.com>
2022-05-16 23:31:32 +0900

    ADD 19강 22.05.16

--- src/main/java/hello/hellospring2/SpringConfig.java
+++ src/main/java/hello/hellospring2/SpringConfig.java
@@ -1,15 +1,22 @@
 package hello.hellospring2;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import hello.hellospring2.repository.JdbcMemberRepository;
 import hello.hellospring2.repository.MemberRepository;
-import hello.hellospring2.repository.MemoryMemberRepository;
 import hello.hellospring2.service.MemberService;
 
 @Configuration
 public class SpringConfig {
-	
+	private final DataSource dataSource;
+
+	public SpringConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
 	@Bean
 	public MemberService memberService() {
 		return new MemberService(memberRepository());
@@ -17,7 +24,7 @@ public class SpringConfig {
 
 	@Bean
 	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+// return new MemoryMemberRepository();
+		return new JdbcMemberRepository(dataSource);
 	}
-
 }
